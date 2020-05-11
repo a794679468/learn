715f824c5cfcbcdde89b4d57a495c7153836dd33
dgq <dgq@DESKTOP-1RG9206>
2020-05-11 10:30:18 +0800

    无法自动部署修改

--- learn/src/main/java/com/dgq/consumer/webservice/CxfWebService.java
+++ learn/src/main/java/com/dgq/consumer/webservice/CxfWebService.java
@@ -6,14 +6,14 @@ import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
-import org.junit.Test;
+//import org.junit.Test;
 
 import com.dgq.consumer.service.MyWebService;
 
 public class CxfWebService{
 	
 	//动态调用
-	@Test
+//	@Test
 	public void test(){
 		
 		JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
