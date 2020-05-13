c1fb7b1981ca4e986bdac8ceefd7d428d97550ae
dgq <dgq@DESKTOP-1RG9206>
2020-05-13 16:29:33 +0800

    修改

--- learn/src/main/java/com/dgq/consumer/service/impl/LuckServiceImpl.java
+++ learn/src/main/java/com/dgq/consumer/service/impl/LuckServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.cxf.common.util.CollectionUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
