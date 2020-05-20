b551770d37a286779e50036a49e676ac46958a30
dgq <dgq@DESKTOP-1RG9206>
2020-05-20 11:19:51 +0800

    阿飞

--- learn/src/main/java/com/dgq/consumer/job/EverdayLuckPredictions.java
+++ learn/src/main/java/com/dgq/consumer/job/EverdayLuckPredictions.java
@@ -24,7 +24,7 @@ public class EverdayLuckPredictions{
 	 * @throws Exception 
 	 */
 //	@Scheduled(cron = "0 0 8 * * 1,3,5")
-	@Scheduled(cron = " 0 20 11 0/1 * ?")
+	@Scheduled(cron = " 0 59 11 0/1 * ?")
     public void predictions() throws Exception{
 		String luck = LuckUtil.printCP("SSQ");
         logger.info("测试周是否执行" + LocalDate.now());
